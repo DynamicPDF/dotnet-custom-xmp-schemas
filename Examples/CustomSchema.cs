509b5f36d5d31e934c3f2761cfa2691af9ed92e4
Andrew Cochran <andrew@dynamicpdf.com>
2019-06-04 08:52:27 -0400

    Updated time formats

--- Examples/CustomSchema.cs
+++ Examples/CustomSchema.cs
@@ -18,9 +18,9 @@ namespace CustomXmpSchemas.Examples
         {
             xmpWriter.BeginDescription("http://ns.adobe.com/xap/1.0/", "xmp", " ");
             if (userName != null) xmpWriter.Draw("\t\t<xmp:CreatedBy>" + userName + "</xmp:CreatedBy>\n");
-            xmpWriter.Draw("\t\t<xmp:DateCreated>" + creationDate.ToUniversalTime() + "</xmp:DateCreated>\n");
+            xmpWriter.Draw("\t\t<xmp:DateCreated>" + creationDate.ToString("yyyy-MM-dd'T'HH:mm:sszzz") + "</xmp:DateCreated>\n");
             xmpWriter.Draw("\t\t<xmp:CreatorTool>" + xmpWriter.Producer + "</xmp:CreatorTool>\n");
-            xmpWriter.Draw("\t\t<xmp:MetadataDate>" + xmpWriter.Date.ToUniversalTime() + "</xmp:MetadataDate>\n");
+            xmpWriter.Draw("\t\t<xmp:MetadataDate>" + xmpWriter.Date.ToLocalTime().ToString("yyyy-MM-dd'T'HH:mm:sszzz") + "</xmp:MetadataDate>\n");
             xmpWriter.EndDescription();
         }
     }
